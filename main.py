6fc667ba83ed8d40da500aaaf48920503243a22e
nifooo <nifo2013@gmx.de>
2025-11-25 13:06:19 +0100

    3rd - wip commit

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ from logger import log_state
 
 
 def main():
+    class Player: 
+        def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT):
+            
+            x = SCREEN_WIDTH / 2
+            y = SCREEN_HEIGHT / 2
+
+
+
+    
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
@@ -16,9 +25,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        Player.draw(screen)
         screen.fill("black")
         pygame.display.flip()
+     
         clock.tick(60)
         dt = clock.tick() / 1000
        # print(dt)
