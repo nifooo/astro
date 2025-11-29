cebe10ed657a4b4bba399912649492be630aec77
nifooo <nifo2013@gmx.de>
2025-11-29 21:28:26 +0100

    final commit

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE_SECONDS, ASTEROID_MAX_RADIUS
 from circleshape import CircleShape
+from logger import log_event
 import pygame # type: ignore
+import random
 
 
 class Asteroid(CircleShape):
@@ -12,3 +14,21 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            pass
+        else:
+            log_event("asteroid_split")
+            degree = random.uniform(20, 50)
+            new_vector_1 = self.velocity.rotate(degree)
+            new_vector_2 = self.velocity.rotate(-degree)
+            new_radius_self = self.radius - ASTEROID_MIN_RADIUS
+            asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius_self)
+            asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius_self)
+            asteroid_1.velocity = new_vector_1 * 1.2
+            asteroid_2.velocity = new_vector_2 * 1.2
+            
+
+            
